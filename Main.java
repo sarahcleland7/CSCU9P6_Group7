26bb483fc2181c02234acab9272de3c2c8973cd3
Matthew Duncan <47351696+Matthew-Duncan@users.noreply.github.com>
2021-02-18 20:00:36 +0000

    Revert "Update Main.java"

--- Main.java
+++ Main.java
@@ -13,8 +13,8 @@ public class Main {
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model); // Create first controller
         Controller2 c2 = new Controller2(model, 40, 200); // Create second controller
- //       Controller2 c3 = new Controller2(model, 40, 360); // Create second controller, we don't need controller2 showing up 3 times.
- //       Controller2 c4 = new Controller2(model, 40, 520); // Create second controller
+        Controller2 c3 = new Controller2(model, 40, 360); // Create second controller
+        Controller2 c4 = new Controller2(model, 40, 520); // Create second controller
         
         
     } // main
